c9e3c2bef9ff2e2e14109856d680a4f835aacf8d
Petya Angelova <bedfordpetya@gmail.com>
2022-12-06 00:20:36 +0200

    footer centered lg screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './public/**/*.{html,js}',
+    "./public/*.html", "./public/js/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        },
+        
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forward',
+      }
+    },
   },
   plugins: [],
 }
